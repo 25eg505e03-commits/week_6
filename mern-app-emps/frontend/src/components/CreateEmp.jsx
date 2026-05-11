56e5208e11d5e74247c0638fdbcc60faf41e6ead
25eg505e03-commits <25eg505e03@anurag.edu.in>
2026-05-11 19:48:23 +0530

    updated

--- mern-app-emps/frontend/src/components/CreateEmp.jsx
+++ mern-app-emps/frontend/src/components/CreateEmp.jsx
@@ -18,7 +18,7 @@ function CreateEmp() {
     try {
       setLoading(true);
       //make HTTP POST req
-      let res = await fetch("https://week-6-bjkg.onrender.com", {
+      let res = await fetch("https://week-6-bjkg.onrender.com/emp-api/employees", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(newEmpObj),
